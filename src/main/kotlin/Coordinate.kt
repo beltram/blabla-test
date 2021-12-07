b1905987d8ba10ee84362257f36ce41200792ec4
beltram.maldant <beltram.maldant@soprabanking.com>
2021-12-07 19:25:34 +0100

    test: add simple tests

--- src/main/kotlin/Coordinate.kt
+++ src/main/kotlin/Coordinate.kt
@@ -3,4 +3,9 @@
  * @param x horizontal axis
  * @param x vertical axis
  */
-class Coordinate(val x: Int, val y: Int)
+class Coordinate(val x: Int, val y: Int) {
+    init {
+        check(x >= 0)
+        check(y >= 0)
+    }
+}
--- src/main/kotlin/Orientation.kt
+++ src/main/kotlin/Orientation.kt
@@ -1,13 +1,25 @@
 /**
- * [Mower] cardinal orientation
+ * [Mower]'s cardinal orientation
  */
 enum class Orientation {
     NORTH, EAST, SOUTH, WEST;
 
     companion object {
         private val values = enumValues<Orientation>()
+        private val len = values.size
     }
 
-    fun rotateRight(): Orientation = TODO()
-    fun rotateLeft(): Orientation = TODO()
+    fun rotateRight() = when (this) {
+        NORTH -> EAST
+        EAST -> SOUTH
+        SOUTH -> WEST
+        WEST -> NORTH
+    }
+
+    fun rotateLeft() = when (this) {
+        NORTH -> WEST
+        WEST -> SOUTH
+        SOUTH -> EAST
+        EAST -> NORTH
+    }
 }
