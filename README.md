4eee4bc6835fee95286df1a6202af2317cd75d81
beltram.maldant <beltram.maldant@soprabanking.com>
2021-12-07 15:53:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### execute tests
+
+```bash
+./gradlew test
+```
---
+++ src/test/kotlin/MowerTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+internal class MowerTest {
+
+    @Test
+    fun `should compile`() {
+
+    }
+}
